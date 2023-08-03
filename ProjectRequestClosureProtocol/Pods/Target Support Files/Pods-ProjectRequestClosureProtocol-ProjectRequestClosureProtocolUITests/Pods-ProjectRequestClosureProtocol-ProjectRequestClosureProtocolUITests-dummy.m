c0106901a919cf32b95c2786fdddddb42ef1ca0b
Ana Paula Silva de Sousa <>
2023-08-02 21:45:13 -0300

    added pods

---
+++ ProjectRequestClosureProtocol/Pods/Target Support Files/Pods-ProjectRequestClosureProtocol-ProjectRequestClosureProtocolUITests/Pods-ProjectRequestClosureProtocol-ProjectRequestClosureProtocolUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ProjectRequestClosureProtocol_ProjectRequestClosureProtocolUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ProjectRequestClosureProtocol_ProjectRequestClosureProtocolUITests
+@end
